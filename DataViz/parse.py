44d947759a6c4889273aac721280d4927c0cf77f
J <funkyfujii@gmail.com>
2017-08-17 19:41:22 -0400

    initial commit - CSV parse complete

---
+++ DataViz/parse.py
@@ -0,0 +1,37 @@
+import csv
+
+# Put the full path to your CSV/Excel file here
+MY_FILE = "../data/sample_sfpd_incident_all.csv"
+
+def parse(raw_file, delimiter):
+    """ Parses a raw CSV file to a JSON-line object"""
+
+    # Open CSV file
+    opened_file = open(raw_file)
+
+    # Read CSV file
+    csv_data = csv.reader(opened_file, delimiter=delimiter)
+
+    # Build a data structure to return parsed_data
+    parsed_data = []
+
+    fields = csv_data.next()
+
+    for row in csv_data:
+        parsed_data.append(dict(zip(fields, row)))
+
+    # Close CSV file
+
+    opened_file.close()
+
+    return parsed_data
+
+def main():
+    # Call our parse function and give it the needed parameters
+    new_data = parse(MY_FILE, ",")
+
+    # Let's see what the data looks like!
+    print(new_data)
+
+if __name__ == "__main__":
+    main()
